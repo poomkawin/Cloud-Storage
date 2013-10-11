ba6bab543f3e5b4be0f807c2dd235bd77ab85e68
Sinderella <rawsec@gmail>
2013-10-11 19:59:16 +0700

    Add String and Bytes support

--- AOOP Project/AOOP/src/Client/hash.java
+++ AOOP Project/AOOP/src/Client/hash.java
@@ -29,13 +29,64 @@ public class hash {
 	    	return null;
 	    }
 	}
+	
+	public static String sha1(String input) throws Exception {
+		final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+
+	    try{
+	    	InputStream is = new ByteArrayInputStream(input.getBytes());
+	    	final byte[] buffer = new byte[1024];
+	    	for (int read = 0; (read = is.read(buffer)) != -1;) {
+	    		messageDigest.update(buffer, 0, read);
+	    	}
+	    }catch(Exception e){
+	    	
+	    }
+
+	    // Convert the byte to hex format
+	    try{
+	    	Formatter formatter = new Formatter();
+	    	for (final byte b : messageDigest.digest()) {
+	    		formatter.format("%02x", b);
+	    	}
+	    	return formatter.toString();
+	    }catch(Exception e){
+	    	return null;
+	    }
+	}
+	
+	public static String sha1(byte[] bytes) throws Exception {
+		final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+
+	    try{
+	    	InputStream is = new ByteArrayInputStream(bytes);
+	    	final byte[] buffer = new byte[1024];
+	    	for (int read = 0; (read = is.read(buffer)) != -1;) {
+	    		messageDigest.update(buffer, 0, read);
+	    	}
+	    }catch(Exception e){
+	    	
+	    }
+
+	    // Convert the byte to hex format
+	    try{
+	    	Formatter formatter = new Formatter();
+	    	for (final byte b : messageDigest.digest()) {
+	    		formatter.format("%02x", b);
+	    	}
+	    	return formatter.toString();
+	    }catch(Exception e){
+	    	return null;
+	    }
+	}
 
 	public static void main(String[] args) {
-		File f = new File("/Users/Sinderella/Desktop/test");
+//		File f = new File("/Users/Sinderella/Desktop/test");
+		String f = "Hello";
 		try {
 			System.out.println(sha1(f));
+			System.out.println(sha1(f.getBytes()));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
