477de487a7cf16fa8d6d9d2f3e4ba8c88b65b1b5
Sinderella <rawsec@gmail>
2013-10-12 10:51:04 +0700

    Can be accept many client as much as possible
    The limitation of 1 peer 1 port has been fixed.

--- AOOP Project/AOOP/src/Server/serverSocket.java
+++ AOOP Project/AOOP/src/Server/serverSocket.java
@@ -3,49 +3,27 @@ package Server;
 import java.io.*;
 import java.net.*;
 
-public class serverSocket extends Thread{
-	private ServerSocket sock;
-	private String recv;
-	private String recvHash;
-	private String content;
+public class serverSocket{
+	static ServerSocket socket;
+	static int port = 4444;
 	
-	serverSocket(int port) throws Exception{
-		sock = new ServerSocket(port);
-	}
-	public void run(){
-		while(true){
-			try{
-				System.out.println("Waiting for client on port " + sock.getLocalPort() + "...");
-				Socket server = sock.accept();
-				System.out.println("Just connected to " + server.getRemoteSocketAddress());
-				DataInputStream in = new DataInputStream(server.getInputStream());
-				int byteLength = in.readInt();
-				byte[] bytes = new byte[byteLength];
-				in.readFully(bytes);
-				recv = new String(bytes, "UTF-8");
-				recvHash = recv.substring(0, 40);
-				content = recv.substring(40);
-				DataOutputStream out = new DataOutputStream(server.getOutputStream());
-				if(hash.sha1(content).equals(recvHash)){
-					out.writeUTF("Okay");
-					System.out.println("Okay");
-//					out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
-					server.close();
-				}else{
-					out.writeUTF("Error");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
 	public static void main(String [] args){
-		int port = 4444;//Integer.parseInt(args[0]);
-		try{
-			Thread t = new serverSocket(port);
-			t.start();
-		}catch(Exception e){
-			e.printStackTrace();
+		try {
+			socket = new ServerSocket(port);
+			System.out.println("Bound to port: " + port);
+		} catch (IOException e) {
+			System.out.println("Cannot bind to port: " + port);
+			System.exit(0);
+		}
+
+		while (true) {
+			try {
+				Socket s = socket.accept();
+				System.out.println("New Client: "+s.getInetAddress().toString());
+				(new Thread(new clientHandler(s))).start();
+			} catch (Exception e) {
+				System.out.println("Failed to accept client");
+			}
 		}
 	}
 }
