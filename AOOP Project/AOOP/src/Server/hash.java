5edf6c8089f59eba32a7447231bd24b5282b1f62
Sinderella <rawsec@gmail>
2013-10-12 15:58:03 +0700

    Optimized

--- AOOP Project/AOOP/src/Server/hash.java
+++ AOOP Project/AOOP/src/Server/hash.java
@@ -14,12 +14,7 @@ public class hash {
 	    	for (int read = 0; (read = is.read(buffer)) != -1;) {
 	    		messageDigest.update(buffer, 0, read);
 	    	}
-	    }catch(Exception e){
-	    	
-	    }
 
-	    // Convert the byte to hex format
-	    try{
 	    	Formatter formatter = new Formatter();
 	    	for (final byte b : messageDigest.digest()) {
 	    		formatter.format("%02x", b);
@@ -39,12 +34,7 @@ public class hash {
 	    	for (int read = 0; (read = is.read(buffer)) != -1;) {
 	    		messageDigest.update(buffer, 0, read);
 	    	}
-	    }catch(Exception e){
-	    	
-	    }
 
-	    // Convert the byte to hex format
-	    try{
 	    	Formatter formatter = new Formatter();
 	    	for (final byte b : messageDigest.digest()) {
 	    		formatter.format("%02x", b);
@@ -56,39 +46,11 @@ public class hash {
 	}
 	
 	public static String sha1(byte[] bytes) throws Exception {
-		final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
-
-	    try{
-	    	InputStream is = new ByteArrayInputStream(bytes);
-	    	final byte[] buffer = new byte[1024];
-	    	for (int read = 0; (read = is.read(buffer)) != -1;) {
-	    		messageDigest.update(buffer, 0, read);
-	    	}
-	    }catch(Exception e){
-	    	
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		Formatter formatter = new Formatter();
+	    for (byte b : messageDigest.digest(bytes)) {
+	        formatter.format("%02x", b);
 	    }
-
-	    // Convert the byte to hex format
-	    try{
-	    	Formatter formatter = new Formatter();
-	    	for (final byte b : messageDigest.digest()) {
-	    		formatter.format("%02x", b);
-	    	}
-	    	return formatter.toString();
-	    }catch(Exception e){
-	    	return null;
-	    }
+		return formatter.toString();
 	}
-
-	public static void main(String[] args) {
-//		File f = new File("/Users/Sinderella/Desktop/test");
-		String f = "Hello";
-		try {
-			System.out.println(sha1(f));
-			System.out.println(sha1(f.getBytes()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 }
